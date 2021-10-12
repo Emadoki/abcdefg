48873c1a4349034181dfb945181e49f98d43f0b2
Christson Fung <Christson.Fung@dyson.com>
2021-10-12 15:31:36 +0800

    efaef

--- app/src/main/java/com/example/hilttest/MainActivity.kt
+++ app/src/main/java/com/example/hilttest/MainActivity.kt
@@ -32,6 +32,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+
+
+        // comment 2
         return true
     }
 
